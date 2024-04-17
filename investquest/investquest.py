81ceb095daec317dd654781e0858a536add2f7ac
joacatar <143612564+joacatar@users.noreply.github.com>
2024-04-17 13:04:16 -0400

    Added a page to show the info for the api call and fixed minor issues on other pages

--- investquest/investquest.py
+++ investquest/investquest.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask import flash, abort, session, jsonify, Blueprint
+import yfinance as yf
+
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -29,6 +31,23 @@ def login():
 @bp.route('/your_account')
 def your_account():
     return render_template('your-account.html')
+
+
+@bp.route('/stock/<symbol>')
+def get_stock_page(symbol):
+    try:
+        stock = yf.Ticker(symbol)
+        for key, value in stock.info.items():
+            print(key, ":", value)
+        stock_info = {
+            "regularMarketPrice": stock.info.get('previousClose', 'N/A'),
+            "previousClose": stock.info.get('previousClose', 'N/A'),
+            "marketCap": stock.info.get('previousClose', 'N/A')
+        }
+        print(stock_info)
+        return render_template('stock.html', symbol=symbol, stock_info=stock_info)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 
